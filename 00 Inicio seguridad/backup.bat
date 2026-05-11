707545ed7281c4bf32e79e79331fac6a8d202415
Hipea3 <128092784+Hipea3@users.noreply.github.com>
2026-05-11 04:50:26 -0300

    Fixv1.1.2

--- 00 Inicio seguridad/backup.bat
+++ 00 Inicio seguridad/backup.bat
@@ -5,10 +5,10 @@ echo   INICIANDO PROTOCOLO DE SEGURIDAD
 echo ===================================================
 echo.
 echo 1. Creando copia de seguridad del BCD...
-bcdedit /export C:\backup_bcd_optimizacion
+bcdedit /export "C:\backup_bcd_optimizacion"
 echo.
 echo 2. Creando punto de restauracion de Windows...
-Wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "Optimizacion_PC_Service", 100, 7
+powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Checkpoint-Computer -Description 'Optimizacion_PC_Service' -RestorePointType 'MODIFY_SETTINGS'"
 echo.
 echo Proceso de seguridad completado. Ya puedes continuar.
 pause
